9243b4e0a8d5a8206ebd321cc4096c70667dcd74
André <tdc@gmx.ch>
2018-03-12 18:27:43 +0100

    tim eto go cooking...

--- app/core/info/info.component.ts
+++ app/core/info/info.component.ts
@@ -9,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ELDInfoComponent implements OnInit {
-    constructor() { }
+    constructor() {
+        console.log('welcome to the info component');
+     }
 
     ngOnInit() { }
 }
