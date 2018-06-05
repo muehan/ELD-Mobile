1f28ecd62a4bbb71a20574247223b19bd09b11d0
André <tdc@gmx.ch>
2018-06-05 18:29:47 +0200

    google.services added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
 import { EventData, Observable } from "data/observable";
+import firebase = require("nativescript-plugin-firebase");
 
 @Component({
   selector: "my-app",
@@ -9,7 +10,22 @@ import { EventData, Observable } from "data/observable";
     'app.component.scss'
   ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor() { }
+
+  ngOnInit(): void {
+    firebase.init({
+      // Optionally pass in properties for database, authentication and cloud messaging,
+      // see their respective docs.
+      
+    }).then(
+      instance => {
+        console.log("firebase.init done");
+      },
+      error => {
+        console.log(`firebase.init error: ${error}`);
+      }
+    );
+  }
 }
